f5b464e0cf01df2249ad64368a1908368252f753
Minh Chau <xiu.chau@gmail.com>
2017-02-15 09:55:24 -0800

    Test SimpleMap component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
-import { GoogleMap, Marker, SearchBox } from "react-google-maps";
+// import { GoogleMap, Marker, SearchBox } from "react-google-maps";
+import SimpleMap from './components/SimpleMap/SimpleMap';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-    }
+      markers: [{
+        position: {
+          lat: 25.0112183,
+          lng: 121.52067570000001,
+        },
+        key: `Taiwan`,
+        defaultAnimation: 2,
+      }],
+      test: 'hi'
+    };
+
+    this.logOut = this.logOut.bind(this);
   }
 
+  logOut() {
+    console.log(this.state.markers);
+  }
 
   render() {
     return (
       <div>
-        <SimpleMap />
+        <button
+          onClick={this.logOut}
+        >
+          Console Log
+        </button>
+        <SimpleMap
+          markers={this.state.markers}
+          test="hello"
+        />
       </div>
     );
   }
