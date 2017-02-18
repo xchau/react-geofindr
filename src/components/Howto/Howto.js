b78906da5a4136c623b8be364da71aff2af0f7a1
Minh Chau <xiu.chau@gmail.com>
2017-02-18 15:56:49 -0800

    Create GH repo

--- src/components/Howto/Howto.js
+++ src/components/Howto/Howto.js
@@ -19,11 +19,25 @@ function Howto(props) {
         <div className="fl f5 w-80 Howto-RightContainer">
           <section>
             <h3 className="b Howto-Title">How to Play GeoFindr</h3>
-            <p>GeoFindr is a less fleshed-out clone of <a className="Howto-Link" href="https://geoguessr.com">GeoGuessr</a>. Your goal in GeoFindr is to guess which city in the world the clues provided allude to.</p>
-            <p>The hints may contain useful bits of information such as a capital city or population count to help you on your way. </p>
-            <p>But sometimes taking a look at the Google Street View might be more helpful. Funny how there are no street labels though. ;)</p>
-            <p>Once you have a good hunch, pin your guess to the map and prepare for victory!</p>
-            <p>Your total points awarded will be determined by how close your guess to the given city. Not satisfied with your score? Try again!</p>
+            <p>
+              GeoFindr is a less fleshed-out clone of <a className="Howto-Link" href="https://geoguessr.com">GeoGuessr</a>. Your goal in GeoFindr is to guess which city in the world the clues provided allude to.
+            </p>
+              <br />
+            <p>
+              The hints may contain useful bits of information such as a capital city or population count to help you on your way.
+            </p>
+              <br />
+            <p>
+              But sometimes taking a look at the Google Street View might be more helpful. Funny how there are no street labels though. ;)
+            </p>
+              <br />
+            <p>
+              Once you have a good hunch, pin your guess to the map and prepare for victory!
+            </p>
+              <br />
+            <p>
+              Your total points awarded will be determined by how close your guess to the given city. Not satisfied with your score? Try again!
+            </p>
           </section>
         </div>
       </div>
